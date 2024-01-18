300675c134ec2aa08bc194ecb49b6a7b03ffbd2e
yanghaiji <hai.ji.yang@ibm.com>
2024-01-18 09:36:19 +0800

    init

---
+++ src/main/java/com/javayh/chechstyle/DemoService.java
@@ -0,0 +1,25 @@
+package com.javayh.chechstyle;
+
+import java.io.SerializablePermission;
+import java.util.concurrent.ScheduledExecutorService;
+import sun.net.httpserver.HttpServerImpl;
+
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author hai ji
+ * @version 1.0.0
+ * @since 2024-01-18
+ */
+public class DemoService {
+
+    public void test(String s1, String s2, String s3, HttpServerImpl server,
+                     SerializablePermission serializablePermission, ScheduledExecutorService scheduledExecutorService) {
+        String sub = "ww";
+    }
+
+
+}
